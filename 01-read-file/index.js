206d2b5b72067823db5bb71b8d7bcdec6eeeb655
olgakaras <olga.karas123@gmail.com>
2021-11-05 21:34:24 +0300

    feat: add Reading a file with content output to the console

---
+++ 01-read-file/index.js
@@ -0,0 +1,9 @@
+const fs = require('fs');
+const path = require('path');
+
+const fileWay = path.join(__dirname, 'text.txt');
+
+const readText = fs.createReadStream(fileWay, 'utf-8');
+readText.on( 'data', function(text) {
+    console.log(text);
+})
