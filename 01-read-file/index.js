0867e475de5fe5f112bd693e840ef7cdd15129db
olgakaras <olga.karas123@gmail.com>
2021-11-08 18:12:58 +0300

    feat: add writing console input to a file

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -4,6 +4,7 @@ const path = require('path');
 const fileWay = path.join(__dirname, 'text.txt');
 
 const readText = fs.createReadStream(fileWay, 'utf-8');
-readText.on( 'data', function(text) {
-    console.log(text);
+readText.on( 'data', function(chunk) {
+    console.log(chunk);
 })
+
---
+++ 02-write-file/index.js
@@ -0,0 +1,28 @@
+const fs = require('fs');
+const path = require('path');
+const readline = require('readline');
+
+const fileWay = path.join(__dirname, 'text2.txt');
+
+const writeText = fs.createWriteStream(fileWay);
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
+
+let array = [];
+console.log("What do you want write here?");
+rl.on('line', (line) => {
+    if (line === 'exit') {
+        rl.close();
+    }
+    else {
+        writeText.write(line + '\n');
+        console.log('You can write more... or click on "Cntl + C" or write "exit" ti exit')
+    }
+});
+
+rl.on('close', () => {
+    console.log("Bye!");
+});
