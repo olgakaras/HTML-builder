8f4a39f34cdaa68c2ccca77b4af355548d6cda49
olgakaras <olga.karas123@gmail.com>
2021-11-08 19:58:48 +0300

    feat: add copy folder

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,24 @@
+const fs = require('fs');
+const path = require('path');
 
+const newFolderWay = path.join(__dirname, 'files-copy');
+const daseFolderWay = path.join(__dirname, 'files');
+
+fs.mkdir(newFolderWay, {recursive: true}, err => {
+    if(err) throw err; 
+    console.log('Folder create!');
+ });
+
+ fs.readdir(daseFolderWay, (err, files) => {
+    if(err) throw err;
+
+    files.forEach(file => {
+        let fileWay = path.join(daseFolderWay, file);
+        let fileWayCopy = path.join(newFolderWay, file);
+
+        fs.copyFile(fileWay, fileWayCopy, err => {
+            if(err) throw err;
+            console.log('Done!');
+        });
+    });
+ });
