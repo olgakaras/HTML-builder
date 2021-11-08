4b5938be2fccf1f2e2c4557ea79512b81732a971
olgakaras <olga.karas123@gmail.com>
2021-11-08 20:33:08 +0300

    feat: add assembly of css bundle

---
+++ 05-merge-styles/index.js
@@ -0,0 +1,24 @@
+const path = require("path");
+const fs = require("fs");
+
+const styleFolderWay = path.join(__dirname, "styles");
+const newFileWay = path.join(__dirname, "project-dist/bundle.css");
+
+const fileOutput = fs.createWriteStream(newFileWay);
+
+fs.readdir(styleFolderWay, { withFileTypes: true }, (err, files) => {
+    if (err) throw err;
+    files.forEach((file) => {
+        let fileName = path.join(styleFolderWay, file.name.toString());
+        let ext = path.extname(fileName);
+        if (!file.isDirectory()) {
+            if (ext == ".css") {
+                let readon = fs.createReadStream(fileName, "utf-8");
+                readon.on("data", (data) => {
+                    fileOutput.write(data + '\n');
+                    console.log('Done!');
+                });
+            }
+        }
+    });
+});
